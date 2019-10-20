984c3659d5c56cb170392819bf12e186ab5a9774
dsuitematheus <matheus@dsuite.com.br>
2019-10-19 21:03:47 -0300

    Cadastro de produtos

---
+++ src/br/com/projeto/jdbc/ConnectionFactory.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projeto.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+
+    public Connection getConnection() {
+
+        try {
+//            return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3309/bdvendas", "usuariocurso", "123");
+        	  return DriverManager.getConnection("jdbc:mysql://localhost:3306/bdvendas?useTimezone=true&serverTimezone=UTC", "usuario", "123");
+        } catch (Exception erro) {
+            throw new RuntimeException(erro);
+        }
+    }
+}
